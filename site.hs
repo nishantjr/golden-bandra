2e7821d6fe3c5ccb00f1f863d4401338baabba26
Nishant Rodrigues <nishantjr@gmail.com>
2024-12-18 22:15:14 +0000

    Initial sketch of timeline view

--- site.hs
+++ site.hs
@@ -16,34 +16,64 @@ main = hakyll $ do
         route   removeInitialComponent
         compile $ loadImage
 
-    match "src/*.css" $ do
+    match "src/**.css" $ do
         route   $ removeInitialComponent
         compile compressCssCompiler
 
-    match "src/**.md" $ do
+    match ( "src/index.md"
+       .||. "src/dlp-letter/**.md"
+       .||. "src/the-varsity-circle/**.md"
+       .||. "src/dlp-letter/**.md")
+        $ do
         route   $ composeRoutes removeInitialComponent $
                                 setExtension "html"
         compile $ do
             myPandocCompiler
                 >>= loadAndApplyTemplate "templates/default.html" defaultContext
 
+    match "src/timeline/*.md"
+        $ timeline
+
+
     match "templates/*" $ compile templateBodyCompiler
 
-  where
-     myPandocCompiler = pandocCompilerWithTransform readerOptions writerOptions usingSideNotes
-     readerOptions = def { readerExtensions = extensionsFromList readerExtensions }
-     readerExtensions = [
-            Ext_fancy_lists,
-            Ext_fenced_divs,
-            Ext_footnotes,
-            Ext_implicit_figures,
-            Ext_line_blocks,
-            Ext_link_attributes,
-            Ext_simple_tables,
-            Ext_startnum,
-            Ext_smart
-        ]
-     writerOptions = def::WriterOptions
+timeline = do
+        route   $ composeRoutes removeInitialComponent $ setExtension "html"
+        compile $ do
+            id <- getUnderlying
+            myPandocCompiler
+                >>= saveSnapshot "content"
+                >>= loadAndApplyTemplate "templates/timeline.html" (timelineCtx id)
+    where timelineCtx id =
+                listField "periods" (periodCtx id) (loadAllSnapshots "src/timeline/*.md" "content")
+                `mappend` defaultContext
+          periodCtx id =
+                field "current" (\i -> if id == itemIdentifier i then return "current"
+                                                                 else fail "other") `mappend`
+                (field "periodStart" (\i -> do return $ start $ itemIdentifier i)) `mappend`
+                (field "periodEnd"   (\i -> do return $ end   $ itemIdentifier i)) `mappend` defaultContext
+          startEnd id = take 2 $ splitAll "-" $ last $ splitDirectories $ dropExtension $ toFilePath id
+          start id = head $ startEnd id
+          end id = last $ startEnd id
+
+
+myPandocCompiler = pandocCompilerWithTransform readerOptions writerOptions usingSideNotes
+  where readerOptions = def { readerExtensions = extensionsFromList readerExtensions }
+        readerExtensions = [
+               Ext_fancy_lists,
+               Ext_fenced_divs,
+               Ext_footnotes,
+               Ext_implicit_figures,
+               Ext_line_blocks,
+               Ext_link_attributes,
+               Ext_simple_tables,
+               Ext_startnum,
+               Ext_smart
+           ]
+        writerOptions = def::WriterOptions
+
+
+
 
 --------------------------------------------------------------------------------
 
---
+++ src/timeline/1725-1750-transition-to-british.md
@@ -0,0 +1,13 @@
+---
+title: Transition to British Authority
+---
+
+During the latter half of the 19th century, Bandra underwent significant changes
+under British rule, as urban development, infrastructure improvements, and
+migrations transformed the area. During the latter half of the 19th century,
+Bandra underwent significant changes under British rule, as urban development,
+infrastructure improvements, and migrations transformed the area. During the
+latter half of the 19th century, Bandra underwent significant changes
+under British rule, as urban development, infrastructure improvements, and
+migrations transformed the area.
+
