ca49108bc890840ea3abb630e09e4e9bff18b772
Nishant Rodrigues <nishantjr@gmail.com>
2025-01-18 19:47:11 +0530

    Replace hardcoded list of articles with dynamically generated one
    We do not yet filter by period

--- site.hs
+++ site.hs
@@ -24,10 +24,7 @@ main = hakyll $ do
         route   $ removeInitialComponent
         compile compressCssCompiler
 
-    match ( "src/index.md"
-       .||. "src/gb/**.md"
-       .||. "src/st-andrew-book/**.md"
-       )
+    match "src/index.md"
         $ do
         route   $ composeRoutes removeInitialComponent $
                                 setExtension "html"
@@ -35,11 +32,20 @@ main = hakyll $ do
             myPandocCompiler
                 >>= loadAndApplyTemplate "templates/default.html" defaultContext
 
-    match "src/timeline/*.md"
-        $ timeline
+    match articlePattern article
 
-
+    match "src/timeline/*.md" timeline
     match "templates/*" $ compile templateBodyCompiler
+
+articlePattern = "src/gb/**.md" .||. "src/st-andrew-book/**.md"
+article = do
+        route   $ composeRoutes removeInitialComponent $ setExtension "html"
+        compile $ do
+            id <- getUnderlying
+            myPandocCompiler
+                >>= saveSnapshot "content"
+                >>= loadAndApplyTemplate "templates/default.html" defaultContext
+                >>= relativizeUrls
 
 timeline = do
         route   $ composeRoutes removeInitialComponent $ setExtension "html"
@@ -50,13 +56,15 @@ timeline = do
                 >>= loadAndApplyTemplate "templates/timeline.html" (timelineCtx id)
                 >>= relativizeUrls
     where timelineCtx id =
-                listField "periods" (periodCtx id) (loadAllSnapshots "src/timeline/*.md" "content")
-                `mappend` defaultContext
+                listField "periods"  (periodCtx id) (loadAllSnapshots "src/timeline/*.md" "content") `mappend`
+                listField "articles" articleCtx     (loadAllSnapshots articlePattern      "content") `mappend`
+                defaultContext
           periodCtx id =
                 field "current" (\i -> if id == itemIdentifier i then return "current"
                                                                  else fail "other") `mappend`
                 (field "periodStart" (\i -> do return $ start $ itemIdentifier i)) `mappend`
                 (field "periodEnd"   (\i -> do return $ end   $ itemIdentifier i)) `mappend` defaultContext
+          articleCtx = defaultContext
           startEnd id = take 2 $ splitAll "-" $ last $ splitDirectories $ dropExtension $ toFilePath id
           start id = head $ startEnd id
           end id = last $ startEnd id
