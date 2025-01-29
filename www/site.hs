e1bec6cdb701ae9315d3649f3481ae836bb698ea
Nishant Rodrigues <nishantjr@gmail.com>
2025-01-29 10:22:10 +0530

    site.hs: Add types to all top-levels

--- www/site.hs
+++ www/site.hs
@@ -28,64 +28,76 @@ main = hakyllWith (def {providerDirectory = ".."}) $ do
     match "index.md" $ do
         route   $ setExtension "html"
         compile $ listingCompiler
-    match "periods/*.md" $ do
+    match periodPattern $ do
         route   $ setExtension "html"
         compile $ listingCompiler
 
     match "www/templates/*" $ compile templateBodyCompiler
 
 
+--- Items
+
+itemPattern :: Pattern
 itemPattern = "items/**.md"
+
+itemCtx :: Context String
+itemCtx = defaultContext
+
+itemCompiler :: Compiler (Item String)
 itemCompiler =
     do id <- getUnderlying
        mdCompiler
          >>= saveSnapshot "content"
-         >>= loadAndApplyTemplate "www/templates/item.html" defaultContext
+         >>= loadAndApplyTemplate "www/templates/item.html" itemCtx
          >>= loadAndApplyTemplate "www/templates/main.html" defaultContext
          >>= relativizeUrls
 
-listingCompiler =
-    do id <- getUnderlying
-       mdCompiler
-         >>= saveSnapshot "content"
-         >>= loadAndApplyTemplate "www/templates/period.html" (periodsCtx id)
-         >>= loadAndApplyTemplate "www/templates/main.html"   defaultContext
-         >>= relativizeUrls
 
-articleCtx = defaultContext
+--- Periods
 
-periodsCtx id =
-                listField "periods"  (periodCtx id) (loadAllSnapshots "periods/*.md" "content") `mappend`
-                listField "articles" articleCtx     (loadAllSnapshots itemPattern "content") `mappend`
-                defaultContext
-    where periodCtx id =
+periodPattern :: Pattern
+periodPattern = "periods/*.md"
+
+periodCtx :: Identifier -> Context String
+periodCtx id =
                 field "current" (\i -> if id == itemIdentifier i then return "current"
                                                                  else fail "other") `mappend`
                 (field "periodStart" (\i -> do return $ start $ itemIdentifier i)) `mappend`
                 (field "periodEnd"   (\i -> do return $ end   $ itemIdentifier i)) `mappend` defaultContext
-          startEnd id = take 2 $ splitAll "-" $ last $ splitDirectories $ dropExtension $ toFilePath id
+    where startEnd id = take 2 $ splitAll "-" $ last $ splitDirectories $ dropExtension $ toFilePath id
           start id = head $ startEnd id
           end id = last $ startEnd id
 
+
+--- Listings
+
+listingCompiler :: Compiler (Item String)
+listingCompiler =
+    do id <- getUnderlying
+       mdCompiler
+         >>= saveSnapshot "content"
+         >>= loadAndApplyTemplate "www/templates/period.html" (listingCtx id)
+         >>= loadAndApplyTemplate "www/templates/main.html"   defaultContext
+         >>= relativizeUrls
+
+listingCtx id = listField "periods"  (periodCtx id) (loadAllSnapshots "periods/*.md" "content") `mappend`
+                listField "articles" itemCtx        (loadAllSnapshots itemPattern    "content") `mappend`
+                defaultContext
+
+
+--- Markdown
+
+mdCompiler :: Compiler (Item String)
 mdCompiler = pandocCompilerWith readerOptions writerOptions
   where readerOptions = def { readerExtensions = extensionsFromList readerExtensions }
         readerExtensions = [
-               Ext_fancy_lists,
-               Ext_fenced_divs,
-               Ext_footnotes,
-               Ext_implicit_figures,
-               Ext_line_blocks,
-               Ext_link_attributes,
-               Ext_simple_tables,
-               Ext_startnum,
+               Ext_fancy_lists, Ext_fenced_divs, Ext_footnotes, Ext_implicit_figures,
+               Ext_line_blocks, Ext_link_attributes, Ext_simple_tables, Ext_startnum,
                Ext_smart
            ]
         writerOptions = def::WriterOptions
 
-
-
-
---------------------------------------------------------------------------------
+--- Routing
 
 removeInitialComponent :: Routes
 removeInitialComponent = customRoute $ tailFilePath . toFilePath
